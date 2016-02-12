054a8c779954ee99e79afa02041629eceaf23f88
Patrick Mutwiri <patwiri@gmail.com>
2016-02-12 07:37:20 +0300

    vars and functionality

--- tmpl/default.php
+++ tmpl/default.php
@@ -14,18 +14,25 @@ $doc = JFactory::getDocument();
 JHtml::_('bootstrap.framework');
 $module = JModuleHelper::getModule( 'mod_loader' );
 $path = JURI::root().'modules/'.$module->module.'/loader.js';
+$path2 = JURI::root().'modules/'.$module->module.'/loader.css';
 $doc->addScript($path);
+$doc->addStyleSheet($path2);
+$tot = count($loader);
+if($tot < 2) {
+	$loader = $loader[0];
+}
 if(is_array($loader)) {
-	$menu = $loader[0];
-	$count = count($loader[0]);
+	$menu = $loader;
+	$count = count($loader);
 	for ($i=0; $i < $count; $i++) { 
 		$div_id = 'div'.$i;
-		$link = $menu[$i]->link.'&Itemid='.$menu[$i]->id;
-		echo '<div class="col-xs-12 col-sm-6 col-md-3" id="'.$div_id.'">'; ?>
-			<script type="text/javascript">
-				geturl("<?php echo $link ?>","<?php echo $div_id ?>");
-			</script>
-		<?php echo '<div>';
-	}
+		$link = $menu[$i]->link.'&Itemid='.$menu[$i]->id; ?>
+		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 grids" id="<?php echo $div_id ?>"></div>
+		<script type="text/javascript">
+				var url = '<?php echo $link; ?>',div = '<?php echo $div_id ?>';
+				geturl(url,div);
+		</script>
+		<?php 	
+	} 
 } ?>
 </div>
